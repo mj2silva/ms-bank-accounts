58810a7148542cec163450d6f86cb9b75060371e
Manuel Silva <manuel.silvag1@gmail.com>
2024-11-10 23:28:06 -0500

    Updated database configuration

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,22 +1,22 @@
-CREATE TABLE IF NOT EXISTS `customer` (
-    `customer_id` int AUTO_INCREMENT PRIMARY KEY,
-    `name` varchar(100) NOT NULL,
-    `email` varchar(100) NOT NULL,
-    `mobile_number` varchar(20) NOT NULL UNIQUE,
-    `created_at` date NOT NULL,
-    `created_by` varchar(20) NOT NULL,
-    `updated_at` date DEFAULT NULL,
-    `updated_by` varchar(20) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id serial PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    mobile_number varchar(20) NOT NULL UNIQUE,
+    created_at date NOT NULL,
+    created_by varchar(20) NOT NULL,
+    updated_at date DEFAULT NULL,
+    updated_by varchar(20) DEFAULT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `account` (
-    `account_id` int AUTO_INCREMENT PRIMARY KEY,
-    `customer_id` int NOT NULL,
-    `account_number` varchar(20) NOT NULL UNIQUE,
-    `account_type` varchar(100) NOT NULL,
-    `branch_address` varchar(200) NOT NULL,
-    `created_at` date NOT NULL,
-    `created_by` varchar(20) NOT NULL,
-    `updated_at` date DEFAULT NULL,
-    `updated_by` varchar(20) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS account (
+    account_id serial PRIMARY KEY,
+    customer_id int NOT NULL,
+    account_number varchar(20) NOT NULL UNIQUE,
+    account_type varchar(100) NOT NULL,
+    branch_address varchar(200) NOT NULL,
+    created_at date NOT NULL,
+    created_by varchar(20) NOT NULL,
+    updated_at date DEFAULT NULL,
+    updated_by varchar(20) DEFAULT NULL
 );
